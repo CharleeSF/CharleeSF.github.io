639b45b5d3e05026183b3d536af5b85097a8408b
CharleeSF <charleetje@gmail.com>
2017-07-06 18:39:20 +0200

    week6

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // load last written entry on default
     $.ajax({
-        url : "week4.html",
+        url : "week6.html",
         dataType: "text",
         success : function (data) {
             $(".text").html(data);
